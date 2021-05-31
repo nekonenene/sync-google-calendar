ecb663498b84c53218e7b3d26c47264ff5349775
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2021-06-01 00:46:57 +0900

    Fetch events using start-date and end-date

--- cmd/executor.go
+++ cmd/executor.go
@@ -9,28 +9,40 @@ import (
 func Exec() {
 	ParseParameters()
 
+	fmt.Println("まずは同期元の Google カレンダーとの連携をおこないます")
 	service, err := GetService()
 	if err != nil {
 		log.Fatalf("Failed to get a service: %v", err)
 	}
 
-	t := time.Now().Format(time.RFC3339)
-	events, err := service.Events.List("primary").ShowDeleted(false).
-		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+	startTime := params.StartTimeOfRange.Format(time.RFC3339)
+	endTime := params.EndTimeOfRange.Format(time.RFC3339)
+
+	events, err :=
+		service.Events.
+			List("primary").
+			ShowDeleted(false).
+			SingleEvents(true).
+			TimeMin(startTime).
+			TimeMax(endTime).
+			MaxResults(2500).
+			OrderBy("startTime").
+			Do()
+
 	if err != nil {
-		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+		log.Fatalf("Unable to retrieve user's events: %v", err)
 	}
 
-	fmt.Println("今後のイベント：")
 	if len(events.Items) == 0 {
-		fmt.Println("No upcoming events found.")
-	} else {
-		for _, item := range events.Items {
-			date := item.Start.DateTime
-			if date == "" {
-				date = item.Start.Date
-			}
-			fmt.Printf("%v (%v)\n", item.Summary, date)
+		fmt.Println("カレンダーにイベントがありませんでした。終了します")
+	}
+
+	fmt.Println("以下のイベントを同期します：")
+	for _, item := range events.Items {
+		date := item.Start.DateTime
+		if date == "" {
+			date = item.Start.Date
 		}
+		fmt.Printf("%v (%v)\n", item.Summary, date)
 	}
 }
